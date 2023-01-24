8688789153002436eb3dfdbcfd61e6b0e948d037
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-01-24 22:17:48 +0900

    [섹션 6] Bottom Navigation 추가, Navigation Component로 화면 이동 구성

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -4,11 +4,22 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null            // BottomNavigation 에서 Theme의 색을 참조하던 것을 초기화 시킴
+
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
 }
