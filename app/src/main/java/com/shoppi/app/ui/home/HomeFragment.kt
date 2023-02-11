4a6f39ba800f478cf578a66eab2adbcbfcea3b90
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-02-11 23:18:42 +0900

    [섹션9] 카테고리 목록 API에 데이터 요청 구현

--- app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.shoppi.app.*
 import com.shoppi.app.databinding.FragmentHomeBinding
