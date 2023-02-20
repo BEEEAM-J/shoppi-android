546d475a9f322523935a179ce248cd470823701d
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-02-21 01:48:07 +0900

    [섹션10] 세부 카테고리 화면 추천상품 목록 UI 구현

--- app/src/main/java/com/shoppi/app/ui/categorydetail/CategoryDetailFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/categorydetail/CategoryDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ConcatAdapter
 import com.shoppi.app.common.KEY_CATEGORY_ID
 import com.shoppi.app.common.KEY_CATEGORY_LABEL
 import com.shoppi.app.databinding.FragmentCategoryDetailBinding
@@ -27,8 +28,22 @@ class CategoryDetailFragment: Fragment() {
 
         binding.lifecycleOwner = viewLifecycleOwner
 
-        val categoryId = requireArguments().getString(KEY_CATEGORY_ID)
+//        val categoryId = requireArguments().getString(KEY_CATEGORY_ID)
+        setToolBar()
+        setListAdapter()
+
+    }
+
+    private fun setToolBar() {
         val categoryLabel = requireArguments().getString(KEY_CATEGORY_LABEL)
         binding.toolbarCategoryDetail.title = categoryLabel
+    }
+
+    private fun setListAdapter(){
+        // ConcatAdapter : 여러 개의 어댑터를 연결
+        // 여러 가지의 뷰 타입을 하나의 어댑터에 배치 가능
+        val titleAdapter = CategorySectionTitleAdapter()
+        val promotionAdapter = CategoryPromotionAdapter()
+        binding.rvCategoryDetail.adapter = ConcatAdapter(titleAdapter, promotionAdapter)
     }
 }
